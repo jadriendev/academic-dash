8d65895253c87ac77a644fcc4a9c4eddef5c7651
Jadrien Roi Aguilar <ddarkknife@gmail.com>
2026-03-14 00:52:23 +0800

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const togglePassword = document.getElementById('togglePassword');
-const password = document.getElementById('password');
-    togglePassword.addEventListener('click', () => {
-    const type = password.type === 'password' ? 'text' : 'password';
-    password.type = type;
-    togglePassword.classList.toggle('fa-eye');
-    togglePassword.classList.toggle('fa-eye-slash');
-});
